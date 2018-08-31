02c6f30b27c20786a5f7e4677ac838cf7b8aa25c
Matthew Rienzo <caswellrienzo@gmail.com>
2018-08-31 11:47:17 -0500

    Working on enumerating fonts into bitmaps

--- EE356P1mfr/MainWindow.xaml.cs
+++ EE356P1mfr/MainWindow.xaml.cs
@@ -15,11 +15,13 @@ using System.Windows.Shapes;
 using Microsoft.Win32;
 using System.Drawing;
 using System.Drawing.Design;
+using System.Drawing.Imaging;
 
 // This is a WPF project, but I have to include this to be able to
 //   enumerate the fixed-width fonts because .NET has no function for listing
 //   fixed-width font families
 using System.Windows.Forms; // I use the TextRenderer class from this
+using System.Drawing.Drawing2D;
 
 // 13 tall
 // 13 wide
@@ -31,13 +33,27 @@ namespace EE356P1mfr
     public partial class MainWindow : Window
     {
         public ICollection<System.Windows.Media.FontFamily> FixedWidthFonts { get; set; }
+        public Dictionary<int, System.Windows.Media.FontFamily> FontIndexer { get; set; }
+        private Dictionary<int, System.Drawing.FontFamily> FormsFontIndexer { get; set; }
+//        private List<System.Windows.Media.FontFamily> FixedWidthFontsEnumerated { get; set; }
+        private string AvailableASCIIString;
+        private Dictionary<float, Bitmap> ASCIIShades { get; set; }
         private bool ColorOutput;
         public MainWindow()
         {
-            this.ColorOutput = true;
+            // Fill out font list with monospaced fonts available from
+            // System.Drawing.  I believe this will be all installed TrueType fixed-width fonts
             this.FixedWidthFonts = EnumerateFixedWidthFonts();
+
+            // Fill out ASCII enumerators
+            this.AvailableASCIIString = "`1234567890-=~!@#$%^&*()_+qwertyuiop[]\\QWERTYUIOP{}|asdfghjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>?█";
             InitializeComponent();
 
+            // Initialize color buttons/status menu items
+            this.ColorOutput = false;
+            this.mnuOptionsToggle_Click(null, null);
+
+            this.ASCIIShades = this.CalculateFontShades();
             // Really just adding this comment to test how the GitHub plugin for VisualStudio
             // handles dates/commit authors/etc.  !!! This was labeled as the `Sysfunc probe'
             // during commit/push on 8/23/2018.  -rienzo
@@ -84,10 +100,12 @@ namespace EE356P1mfr
             if (this.ColorOutput)
             {
                 mnuItmColorOutputMode.Header = "Color output is ON";
+                mnuColorstatus.Header = "Color";
             }
             else
             {
                 mnuItmColorOutputMode.Header = "Color output is OFF";
+                mnuColorstatus.Header = "Black and white";
             }
         }
 
@@ -115,6 +133,13 @@ namespace EE356P1mfr
         private ICollection<System.Windows.Media.FontFamily> EnumerateFixedWidthFonts()
         {
             ICollection<System.Windows.Media.FontFamily> MonoSpaceFonts = new List<System.Windows.Media.FontFamily>();
+            int index = 0;
+            // Wipe shades
+            //            this.FixedWidthFontsEnumerated = null;
+            this.FontIndexer = new Dictionary<int, System.Windows.Media.FontFamily>();
+            this.FormsFontIndexer = new Dictionary<int, System.Drawing.FontFamily>();
+            string exceptionsFonts = null;
+            string exceptionText = null;
             foreach (System.Drawing.FontFamily ff in System.Drawing.FontFamily.Families)
             {
                 if (ff.IsStyleAvailable(System.Drawing.FontStyle.Regular))
@@ -128,15 +153,32 @@ namespace EE356P1mfr
                     {
                         try
                         {
+                            System.Windows.Media.FontFamily fo = new System.Windows.Media.FontFamily(ff.Name);
                             MonoSpaceFonts.Add(new System.Windows.Media.FontFamily(ff.Name));
+//                            this.FixedWidthFontsEnumerated.Add(new System.Windows.Media.FontFamily(ff.Name));
+                            this.FontIndexer.Add(index, fo);
+                            this.FormsFontIndexer.Add(index, ff);
                         }
-                        catch
+                        catch(Exception e)
                         {
-                            // Skip I guess because the font doesn't exist
+                            exceptionsFonts += ff.Name + "\n";
+                            exceptionText += e.ToString() + "\nException message:" + e.Message + "\n\n";
                         }
+                        index++;
                     }
                 }
 
+            }
+
+            if(exceptionsFonts != null)
+            {
+                MessageBoxResult r = System.Windows.MessageBox.Show("Encountered exception(s) while enumerating fonts!\nThe following font families generated an exception:\n" + exceptionsFonts + "\nSee the exception(s) text(s)?",
+                    "Font exception action", MessageBoxButton.YesNo);
+
+                if(r == MessageBoxResult.Yes)
+                {
+                    System.Windows.MessageBox.Show(exceptionText);
+                }
             }
 
             if(MonoSpaceFonts == null)
@@ -147,5 +189,30 @@ namespace EE356P1mfr
             return MonoSpaceFonts;
         }
         
+        private Dictionary<float, Bitmap> CalculateFontShades()
+        {
+            int fontIndex = cmboFonts.SelectedIndex;
+            
+            System.Drawing.Image bmp = new Bitmap(100, 100);
+            for (int i = 0; i < AvailableASCIIString.Length; i++)
+            {
+                Graphics g = Graphics.FromImage(bmp);
+
+                Font myFont = new Font(FormsFontIndexer[fontIndex], 14, GraphicsUnit.Pixel);
+                SizeF size = g.MeasureString(""+AvailableASCIIString[i], myFont);
+                PointF rect = new PointF(size.Width, size.Height);
+
+                System.Windows.MessageBox.Show("X:" + (int)Math.Ceiling(rect.X) + "\nY:" + (int)Math.Ceiling(rect.Y));
+                System.Windows.Application.Current.Shutdown();
+                System.Drawing.Image outBmp = new Bitmap((int)Math.Ceiling(rect.X), (int)Math.Ceiling(rect.X));
+                g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                g.DrawString("" + AvailableASCIIString[i], myFont, new SolidBrush(System.Drawing.Color.Black), rect);
+            }
+
+
+            return null;
+        }
     }
 }
